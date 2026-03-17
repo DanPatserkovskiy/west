26537d2e695914a3642c5d20231376b1c00e673a
Пацерковский Даниил <patserkovskiy@list.ru>
2026-03-17 19:17:00 +0500

    'Gatling'

--- src/index.js
+++ src/index.js
@@ -51,13 +51,34 @@ class Duck extends Creature {
     swims() {
         console.log('float: both;');
     }
-}
-
+};
 
 // Основа для собаки.
 class Dog extends Creature {
     constructor(name = 'Бандит', power = 3) {
         super(name, power);
+    }
+}
+
+class Gatling extends Creature {
+    constructor(name = 'Гатлинг', power = 6){
+        super(name, power);
+    }
+
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        const oppositeTable = gameContext.oppositePlayer.table;
+        for (let i = 0; i < oppositeTable.length; i++) {
+            taskQueue.push(onDone => this.view.showAttack(onDone));
+            taskQueue.push(onDone => {
+                const oppositeCard = oppositeTable[i];
+                this.dealDamageToCreature(2, oppositeCard, gameContext, onDone);
+            });
+        }
+
+        taskQueue.continueWith(continuation);
     }
 }
 
@@ -77,6 +98,18 @@ class Trasher extends Dog {
     }
 }
 
+/*const seriffStartDeck = [
+    new Duck(),
+    new Duck(),
+    new Duck(),
+    new Gatling(),
+];
+const banditStartDeck = [
+    new Trasher(),
+    new Dog(),
+    new Dog(),
+];*/
+
 // const seriffStartDeck = [
 //     new Duck(),
 //     new Duck(),
@@ -86,7 +119,7 @@ class Trasher extends Dog {
 //     new Dog(),
 // ];
 
-const seriffStartDeck = [
+/*const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
@@ -94,7 +127,7 @@ const seriffStartDeck = [
 ];
 const banditStartDeck = [
     new Trasher(),
-];
+];*/
 
 
 // Колода Шерифа, нижнего игрока.
